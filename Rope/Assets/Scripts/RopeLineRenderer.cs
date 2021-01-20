5a744abcc42d0c65e7463a03d1a8c2e478d9229c
Viscoale <alessandro.visconti97@gmail.com>
2021-01-20 09:28:44 +0100

    Aggiunta line renderer

---
+++ Rope/Assets/Scripts/RopeLineRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeLineRenderer : MonoBehaviour
+{
+    public LineRenderer line;
+    public GameObject Element1;
+    public GameObject Element2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.SetPosition(0, Element1.transform.position);
+        line.SetPosition(1, Element2.transform.position);
+    }
+}
